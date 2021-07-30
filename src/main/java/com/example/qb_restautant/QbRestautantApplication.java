a38bcc93ab8160d0a533f9e70effcb2203343336
VoHoangDuong <duong6046@gmail.com>
2021-07-30 08:24:27 +0700

    update

---
+++ src/main/java/com/example/qb_restautant/QbRestautantApplication.java
@@ -0,0 +1,13 @@
+package com.example.qb_restautant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbRestautantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QbRestautantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qb_restautant/QbRestautantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qb_restautant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbRestautantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
